4734edfd7974e4540325ed9c409f4536c20a70fe
yuyakan <kanbe1365@icloud.com>
2022-03-16 10:37:54 +0900

    Initial Commit

---
+++ bingo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  bingo
+//
+//  Created by 上別縄祐也 on 2022/03/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ bingo/bingoApp.swift
@@ -0,0 +1,17 @@
+//
+//  bingoApp.swift
+//  bingo
+//
+//  Created by 上別縄祐也 on 2022/03/16.
+//
+
+import SwiftUI
+
+@main
+struct bingoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
